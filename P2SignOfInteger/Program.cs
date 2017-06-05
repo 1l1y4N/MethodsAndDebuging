10efe20dfae332bbc99b36a02d897047f5de23a2
1l1y4N <cvetkov.iliyan@gmail.com>
2017-06-05 17:32:36 +0300

    p3 todo

---
+++ P2SignOfInteger/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace P2SignOfInteger
+{
+    class Program
+    {
+        static void PrintSign(int num)
+        {
+            if (num > 0)
+            {
+                Console.WriteLine("The number {0} is positive.", num);
+            }
+            else if (num < 0 )
+            {
+                Console.WriteLine("The number {0} is negative.", num);
+            }
+            else
+            {
+                Console.WriteLine("The number {0} is zero.", num);
+            }
+        }
+        static void Main(string[] args)
+        {
+        int n = int.Parse(Console.ReadLine());
+            PrintSign(n);
+        }
+    }
+}
---
+++ P3PrintingTriangle/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3PrintingTriangle
+{
+    class Program
+    {
+        static void PrintNumbers(int start, int end)
+        {
+            for (int i = start ; i <= end ; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+        static void PrintNumbersReverse(int start, int end)
+        {
+            for (int i = start; i >= end; i--)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                PrintNumbers(1,i);
+            }
+            for (int j = (n-1); j >= 1; j--)
+            {
+                PrintNumbersReverse(j, 1);
+            }
+            
+        }
+    }
+}
